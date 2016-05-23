1f336b726bed010e6c7e69216ed564c8aab73dae
eemmzz <emmziie@gmail.com>
2016-05-23 21:56:33 +0100

    Initial express app commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    path = require('path'),
+    logger = require('morgan'),
+    routes = require('./routes/index'),
+    app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+              message: err.message,
+              error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+        res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
