8b3bf1096651ffdf3b7ce40819490cadb4cfb114
eemmzz <emmziie@gmail.com>
2016-05-24 22:49:20 +0100

    Adding basic socket io setup to project

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
     path = require('path'),
     logger = require('morgan'),
-    routes = require('./routes/index'),
-    app = express();
+    index = require('./routes/index');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
@@ -11,9 +15,19 @@ app.engine('html', require('hbs').__express);
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
 
-// catch 404 and forward to error handler
+io.on('connection', function (socket) {
+    var helloWorld = setInterval(function () {
+        socket.volatile.emit('greeting', 'Hello world!');
+    }, 2000);
+
+    socket.on('disconnect', function () {
+        clearInterval(helloWorld);
+    });
+});
+
+// Catch 404s and pass onto error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -38,4 +52,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-module.exports = app;
+module.exports = { app: app, server: server };
