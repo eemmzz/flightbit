cfd215d97ce95e3defbca7f6bf64d7186b14a761
Emma Maloney <emma.maloney@bbc.co.uk>
2017-04-28 22:35:28 +0100

    Add config file and es6ify index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,22 @@
-module.exports = function(req, res, next) {
-    res.render('index', { title: 'Express' });
+const config = require('config');
+const request = require('request');
+
+module.exports = (req, res, next) => {
+    const options = {
+        url: config.get('opensky.url'),
+        timeout: 2000
+    }
+
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                resolve(body);
+            } else {
+                reject(new Error('Error occurred fetching open sky data'));
+            }
+        });
+    }).then((result) => {
+        console.log(result);
+        res.render('index', { title: 'Welcome to Flightbit' });
+    })
 };
