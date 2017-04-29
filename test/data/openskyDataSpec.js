b4394c559c0a5eb96f119281b87b3711131e4c03
Emma Maloney <emmziie@gmail.com>
2017-04-29 17:07:34 +0100

    Ensure all callsign varieties are tested

--- test/data/openskyDataSpec.js
+++ test/data/openskyDataSpec.js
@@ -16,8 +16,8 @@ describe('openskyData', () => {
 
         it('Should return a key:value map of icao24 and callsign', async () => {
             const mockFlightStatesData = [
-                [ "aa8c39", "UAL252" ],
-                [ "ac494e", "N891CS" ]
+                [ 'aa8c39', 'UAL252' ],
+                [ 'ac494e', 'N891CS' ]
             ];
 
             rewireResets.push(
@@ -27,6 +27,62 @@ describe('openskyData', () => {
             const expected = [
                 { 'UAL252': 'aa8c39' },
                 { 'N891CS': 'ac494e' }
+            ]
+            const result = await openskyData.getCurrentFlightsMap();
+            
+            assert.deepEqual(result, expected);
+        });
+
+        it('Should trim whitespace from callsigns', async () => {
+            const mockFlightStatesData = [
+                [ 'aa56db', '  UAL253' ],
+                [ 'a086ce', ' UAL540  ' ]
+            ];
+
+            rewireResets.push(
+                openskyData.__set__('fetchAllCurrentFlightStates', sinon.stub().returns(mockFlightStatesData))
+            );
+
+            const expected = [
+                { 'UAL253': 'aa56db' },
+                { 'UAL540': 'a086ce' }
+            ]
+            const result = await openskyData.getCurrentFlightsMap();
+            
+            assert.deepEqual(result, expected);
+        });
+
+        it('Should remove flights with null callsigns', async () => {
+            const mockFlightStatesData = [
+                [ 'a92d6d', 'UAL86' ],
+                [ 'ac058a', null ]
+            ];
+
+            rewireResets.push(
+                openskyData.__set__('fetchAllCurrentFlightStates', sinon.stub().returns(mockFlightStatesData))
+            );
+
+            const expected = [
+                { 'UAL86': 'a92d6d' }
+            ]
+            const result = await openskyData.getCurrentFlightsMap();
+            
+            assert.deepEqual(result, expected);
+        });
+
+        it('Should remove flights with empty callsigns', async () => {
+            const mockFlightStatesData = [
+                [ '4d0102', 'CLX810' ],
+                [ 'a086ce', '   ' ],
+                [ 'aa8c1b', '' ]
+            ];
+
+            rewireResets.push(
+                openskyData.__set__('fetchAllCurrentFlightStates', sinon.stub().returns(mockFlightStatesData))
+            );
+
+            const expected = [
+                { 'CLX810': '4d0102' }
             ]
             const result = await openskyData.getCurrentFlightsMap();
             
