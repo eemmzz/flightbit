7bb6f66eaf4e6fd8e50c9d65ee11e166ec43a999
Emma Maloney <emmziie@gmail.com>
2017-04-29 17:29:21 +0100

    Display current data on the view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const request = require('request');
 const openskyData = require('./../lib/data/openskyData');
 
 module.exports = (req, res, next) => {
-    openskyData.getCurrentFlightsMap().then(() => {
-        res.render('index', { title: 'Welcome to Flightbit' });
+    openskyData.getCurrentFlightsMap().then((result) => {
+        const viewData = {
+            title: 'Welcome to Flightbit',
+            data: JSON.stringify(result)
+        }
+        res.render('index', viewData);
     })
 };
